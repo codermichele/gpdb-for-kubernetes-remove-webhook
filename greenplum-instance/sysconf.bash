6c7607bcf535fa9618fd93a71bc9a3db6c91bca8
codermichele <yes_brown@qq.com>
2023-07-21 17:42:52 +0800

    docker stage

--- greenplum-instance/sysconf.bash
+++ greenplum-instance/sysconf.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #install required deb
-sub_path=/greenplum-for-kubernetes/greenplum-instance
+sub_path=/tools
 for deb in `cat ${sub_path}/greenplum_instance_apt_install_order.txt`;do
   deb=${deb#*_}
   dpkg -i ${sub_path}/greenplum-instance-debs/${deb}
