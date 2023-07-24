09dbaa7706f38f1d150ba129e1c9d70a6e0e297a
codermichele <yes_brown@qq.com>
2023-07-24 17:39:14 +0800

    Add deb

---
+++ greenplum-operator/sysconf.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#install required deb
+sub_path=/greenplum-for-kubernetes/greenplum-operator
+for deb in `cat ${sub_path}/greenplum_operator_apt_install_order.txt`;do
+  deb=${deb#*_}
+  dpkg -i ${sub_path}/greenplum-operator-debs/${deb}
+done
