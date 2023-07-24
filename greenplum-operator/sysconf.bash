5bd347680ba8f7079d250f28b4561f0eb4ceadd5
codermichele <yes_brown@qq.com>
2023-07-24 18:04:52 +0800

    Update

--- greenplum-operator/sysconf.bash
+++ greenplum-operator/sysconf.bash
@@ -4,5 +4,5 @@
 sub_path=/greenplum-for-kubernetes/greenplum-operator
 for deb in `cat ${sub_path}/greenplum_operator_apt_install_order.txt`;do
   deb=${deb#*_}
-  dpkg -i ${sub_path}/greenplum-operator-debs/${deb}
+  dpkg -i ${sub_path}/${deb}
 done
