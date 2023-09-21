9a05313fb2a79a04349271a0efbce88a71b248ed
codermichele <124560019+codermichele@users.noreply.github.com>
2023-09-21 14:03:52 +0800

    Update greenplum_command.go

--- greenplum-instance/cmd/startGreenplumContainer/startContainerUtils/cluster/greenplum_command.go
+++ greenplum-instance/cmd/startGreenplumContainer/startContainerUtils/cluster/greenplum_command.go
@@ -24,7 +24,8 @@ func (g *GreenplumCommand) Command(command string, args ...string) *exec.Cmd {
 		"PATH=/usr/local/greenplum-db/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
 		"LD_LIBRARY_PATH=/usr/local/greenplum-db/lib:/usr/local/greenplum-db/ext/python/lib",
 		"MASTER_DATA_DIRECTORY=/greenplum/data-1",
-		"PYTHONHOME=/usr/local/greenplum-db/ext/python",
+		"PYTHONHOME=/usr/lib/python2.7",
+		//"PYTHONHOME=/usr/local/greenplum-db/ext/python",
 		"PYTHONPATH=/usr/local/greenplum-db/lib/python")
 	return cmd
 }
