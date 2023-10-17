84578e91994a3380ff37d2c8cc7a51cd89dab8d8
codermichele <yes_brown@qq.com>
2023-10-17 14:39:35 +0800

    Change PYTHONPATH in greenplum_command.go

--- greenplum-instance/cmd/startGreenplumContainer/startContainerUtils/cluster/greenplum_command.go
+++ greenplum-instance/cmd/startGreenplumContainer/startContainerUtils/cluster/greenplum_command.go
@@ -25,6 +25,6 @@ func (g *GreenplumCommand) Command(command string, args ...string) *exec.Cmd {
 		"LD_LIBRARY_PATH=/usr/local/greenplum-db/lib:/usr/local/greenplum-db/ext/python/lib",
 		"MASTER_DATA_DIRECTORY=/greenplum/data-1",
 		"PYTHONHOME=/usr/local/greenplum-db/ext/python",
-		"PYTHONPATH=/usr/lib/python2.7:/usr/lib/python2.7/plat-x86_64_gnu:/usr/lib/python2.7/lib-tk:/usr/lib/python2.7/lib-old:/usr/lib/python2.7/lib-dynload:/usr/local/lib/python2.7/dist-packages:/usr/lib/python2.7/dist-packages:/usr/local/greenplum-db/lib/python")
+		"PYTHONPATH=/usr/lib/python2.7:/usr/lib/python2.7/plat-x86_64-linux-gnu:/usr/lib/python2.7/lib-tk:/usr/lib/python2.7/lib-old:/usr/lib/python2.7/lib-dynload:/usr/local/lib/python2.7/dist-packages:/usr/lib/python2.7/dist-packages:/usr/local/greenplum-db/lib/python")
 	return cmd
 }
